16d096d2fa3debb01ee233833f21294c480e72a3
Mikkel Rom Engholm <mikkel.engholm@knowit.dk>
2022-09-21 14:20:55 +0200

    Initial commit

---
+++ src/calculator.js
@@ -0,0 +1,8 @@
+function calculate(input) {
+    return input;
+
+    // *** Solution ***
+    // const values = input.split('+');
+    // return parseInt(values[0]) + parseInt(values[1]);
+}
+module.exports = calculate;
---
+++ src/calculator.spec.js
@@ -0,0 +1,5 @@
+const calculate = require('./calculator');
+
+it('should add "1+2" to equal 3', () => {
+    expect(calculate("1+2")).toBe(3);
+});
